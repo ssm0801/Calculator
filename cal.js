c1ccf3eb7be06658edfa9c57cb4e48e8a526d410
ssm0801 <46257802+ssm0801@users.noreply.github.com>
2019-12-27 01:39:41 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,18 @@
+function insert(num){
+    document.form.text.value += num;
+}
+
+function equal(){
+    var exp = document.form.text.value;
+    if(exp)
+        document.form.text.value = eval(exp);
+}
+
+function clean(){
+    document.form.text.value = "";
+}
+
+function back(){
+    var exp = document.form.text.value;
+    document.form.text.value = exp.substring(0,exp.length-1);
+}
